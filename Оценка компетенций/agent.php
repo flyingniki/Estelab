2f2b618eaadc4757b683c6e07404b8ee1a20a60e
Petr Nikitin <flying_niki@mail.ru>
2022-10-28 17:50:57 +0300

    v2

--- Оценка компетенций/agent.php
+++ Оценка компетенций/agent.php
@@ -9,16 +9,20 @@ $container = Service\Container::getInstance();
 
 $by = "id";
 $order = "ASC";
-$clinics_id = [344, 4551, 5073, 4967, 7676, 7389, 4959, 7371, 6531, 5377, 6381, 76777, 7678];
+$ar_ids = [
+    'clinic' => [344, 4551, 5073, 4967, 7676, 7389, 4959, 7371, 6531, 5377, 6381, 76777, 7678],
+    'finance' => [4982]
+];
+foreach ($ar_ids as $dep_name => $ids) {
+    foreach ($ids as $id) {
+        $filter = array("ACTIVE" => "Y", "GROUPS_ID" => array(11), "UF_DEPARTMENT" => $id);
+        $arParams = array("SELECT" => array('UF_DEPARTMENT'));
 
-foreach ($clinics_id as $clinic_id) {
-    $filter = array("ACTIVE" => "Y", "GROUPS_ID" => array(11), "UF_DEPARTMENT" => $clinic_id);
-    $arParams = array("SELECT" => array('UF_DEPARTMENT'));
+        $rsUsers = CUser::GetList($by, $order, $filter, $arParams);
 
-    $rsUsers = CUser::GetList($by, $order, $filter, $arParams);
-
-    while ($user = $rsUsers->Fetch()) {
-        $users_clinic[] = ['ID' => $user['ID'], 'FULL_NAME' => $user['NAME'] . ' ' . $user['LAST_NAME'], 'DEPARTMENT' => $user['UF_DEPARTMENT'][0]];
+        while ($user = $rsUsers->Fetch()) {
+            $users_info[$dep_name][] = ['ID' => $user['ID'], 'FULL_NAME' => $user['NAME'] . ' ' . $user['LAST_NAME'], 'DEPARTMENT' => $user['UF_DEPARTMENT'][0]];
+        }
     }
 }
-print_r($users_clinic);
+print_r($users_info);
