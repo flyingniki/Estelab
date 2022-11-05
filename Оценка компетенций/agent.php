5a53c06255f22e2d372caf7bfba5d916cd395089
Petr Nikitin <flying_niki@mail.ru>
2022-11-05 13:24:47 +0300

    v3

--- Оценка компетенций/agent.php
+++ Оценка компетенций/agent.php
@@ -1,33 +1,106 @@
 <?php
 
-use Bitrix\Crm\Service;
+uuse Bitrix\Crm\Service;
 
 CModule::IncludeModule("crm");
 CModule::IncludeModule("workflow");
 CModule::IncludeModule("bizproc");
 $container = Service\Container::getInstance();
 
-$by = "id";
-$order = "ASC";
-$ar_ids = [
-    'clinic' => [344, 4551, 5073, 4967, 7676, 7389, 4959, 7371, 6531, 5377, 6381, 76777, 7678],
-    'finance' => [4982],
-    'sales department' => [376],
-    'teachers' =>[5241],
-    'it' => [4962],
-    'logistics' => [4962],
-    'marketing' =>[5271]
+$arDepartments = [
+    'ИМ + Клиника' => [4551, 7675, 5073, 4967, 7676, 4959, 7389, 4959, 7371, 6531, 5377, 6381],
+    'Маркетинг + Поддержка Гл. Врача + Тренинг' => [5271, 7409, 7381],
+    'Логистика + Косметика и производство' => [4963, 5080],
+    'Отдел продаж + Косметика + Студия + Оборудование' => [376, 5080, 4966, 7342, 6981, 4914],
+    'Финансы' => [4982],
+    // 'Логистика + Ст. Администратор' => [4963, ], //внести id ст администратора
+    'IT + Финансы + HR + Scrum master' => [4962, 4982, 6163, 346], //внести id скрам-мастера    
 ];
-foreach ($ar_ids as $dep_name => $ids) {
-    foreach ($ids as $id) {
-        $filter = array("ACTIVE" => "Y", "GROUPS_ID" => array(11), "UF_DEPARTMENT" => $id);
-        $arParams = array("SELECT" => array('UF_DEPARTMENT'));
 
-        $rsUsers = CUser::GetList($by, $order, $filter, $arParams);
+$arSelect = ['ID', 'NAME', 'LAST_NAME'];
+foreach ($arDepartments as $depName => $depIds) {
+    foreach ($depIds as $depId) {
+        // echo '<pre>';
+        $arEmployees = CIntranetUtils::GetDepartmentEmployees($depId, false, false, 'Y', $arSelect);
 
-        while ($user = $rsUsers->Fetch()) {
-            $users_info[$dep_name][] = ['ID' => $user['ID'], 'FULL_NAME' => $user['NAME'] . ' ' . $user['LAST_NAME'], 'DEPARTMENT' => $user['UF_DEPARTMENT'][0]];
+        while ($rsEmployees = $arEmployees->fetch()) {
+            $usersIds[$depName][] = $rsEmployees['ID'];
+            $usersIds[$depName] = array_filter($usersIds[$depName]);
         }
+        $managerIds[$depName][] = CIntranetUtils::GetDepartmentManagerID($depId);
+        $managerIds[$depName] = array_filter($managerIds[$depName]);
     }
 }
-print_r($users_info);
+//print_r($managerIds);
+//print_r($usersIds);
+$mergedIds = array_merge_recursive($managerIds, $usersIds);
+foreach ($mergedIds as $depName => $arMergedId) {
+    $arUniqueMergedId = array_unique($arMergedId);
+    $arResIds[$depName] = $arUniqueMergedId;
+    // создаем элемент СП "Группы сотрудников"
+    $Smart_Type_ID = 168;
+    $title = "Сотрудники группы {$depName} @ " . date('d-m-Y');
+
+    $factory = $container->getFactory($Smart_Type_ID);
+
+    $data = [
+        'TITLE' => $title,
+        'UF_CRM_74_1667223668' => $depName,
+        'UF_CRM_74_1667468637' => $arUniqueMergedId
+    ];
+    $item = $factory->createItem($data);
+
+    $res = $item->save();
+    $arEmpGroupIds = $item->getUfCrm_74_1667468637();
+    foreach ($arEmpGroupIds as $key => $empId) {
+        // получаем все ID сотрудников, кроме текущего
+        $arOtherIds = $arEmpGroupIds;
+        unset($arOtherIds[$key]);
+        // создаем элемент СП "Оценка компетенций (коллективная)"
+        $Smart_Type_ID = 130;
+        $factory = $container->getFactory($Smart_Type_ID);
+        $data = [
+			'UF_CRM_78_1667640871' => $depName,
+            'UF_CRM_78_1667470176' => $empId,
+            'UF_CRM_78_1667640513' => $arOtherIds
+        ];
+        $item = $factory->createItem($data);
+        $res = $item->save();
+        $item_id = $res->getId();
+        $workflowTemplateId = 2212;
+        $arErrorsTmp = array();
+        CBPDocument::StartWorkflow(
+            $workflowTemplateId,
+            array("crm", "Bitrix\Crm\Integration\BizProc\Document\Dynamic", "DYNAMIC_" . $Smart_Type_ID . "_" . $item_id),
+            array(),
+            $arErrorsTmp
+        );
+    }
+    // массив ID всех сотрудников
+    foreach ($arUniqueMergedId as $id) {
+        $arEmployeeIds[] = $id;
+    }
+}
+$arEmployeeIds = array_unique($arEmployeeIds);
+
+foreach ($arEmployeeIds as $empId) {
+    // создаем элемент СП "Оценка компетенций (личная)"
+    $Smart_Type_ID = 158;
+    $factory = $container->getFactory($Smart_Type_ID);
+    $data = [
+        'UF_CRM_75_1667471974' => $empId
+    ];
+    $item = $factory->createItem($data);
+    $res = $item->save();
+    $item_id = $res->getId();
+    $workflowTemplateId = 2207;
+    $arErrorsTmp = array();
+    CBPDocument::StartWorkflow(
+        $workflowTemplateId,
+        array("crm", "Bitrix\Crm\Integration\BizProc\Document\Dynamic", "DYNAMIC_" . $Smart_Type_ID . "_" . $item_id),
+        array(),
+        $arErrorsTmp
+    );
+}
+// print_r($arResIds);
+// print_r($arEmployeeIds);
