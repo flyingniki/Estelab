6ad74b476e977b89825eb54060cb86895a1cfcc4
Petr Nikitin <flying_niki@mail.ru>
2022-11-07 21:53:38 +0300

    поправил шаблон вывода компетенций

--- Оценка компетенций/grades.php
+++ Оценка компетенций/grades.php
@@ -2,6 +2,19 @@
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Шкала развития компетенций");
+
+use Bitrix\Crm\Service;
+
+CModule::IncludeModule("crm");
+CModule::IncludeModule("workflow");
+CModule::IncludeModule("bizproc");
+
+$container = Service\Container::getInstance();
+
+$Smart_Type_ID = 144;
+$factory = $container->getFactory($Smart_Type_ID);
+$items = $factory->getItems();
+
 ?>
 
 <style>
@@ -13,20 +26,16 @@ $APPLICATION->SetTitle("Шкала развития компетенций");
     .questions {
         text-align: center;
     }
-
-    img {
-        width: 50%;
-    }
 </style>
 <h1>Шкала развития компетенций</h1>
 <div class="questions">
-    <div class="question-item"><img src="/company/otsenka-kompetentsiy/images/01.jpg" alt=""></div>
-    <div class="question-item"><img src="/company/otsenka-kompetentsiy/images/02.jpg" alt=""></div>
-    <div class="question-item"><img src="/company/otsenka-kompetentsiy/images/03.jpg" alt=""></div>
-    <div class="question-item"><img src="/company/otsenka-kompetentsiy/images/04.jpg" alt=""></div>
-    <div class="question-item"><img src="/company/otsenka-kompetentsiy/images/05.jpg" alt=""></div>
-    <div class="question-item"><img src="/company/otsenka-kompetentsiy/images/06.jpg" alt=""></div>
-    <div class="question-item"><img src="/company/otsenka-kompetentsiy/images/07.jpg" alt=""></div>
+    <?
+    foreach ($items as $item) {
+        $imgId = $item->getUfCrm_76_1667825809();
+        $img = CFile::ShowImage($imgId);
+        ?><div class="question-item"><?= $img ?></div><?
+    }
+    ?>
 </div>
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
